e573c7ab9f3adfc23d62529bb171ccc1a92bff9d
rhong612 <rhong612@gmail.com>
2017-02-19 01:28:26 -0800

    Added Actors to act as the Player/Enemy board

--- Pong/core/src/com/pong/raymondhong/Pong.java
+++ Pong/core/src/com/pong/raymondhong/Pong.java
@@ -6,24 +6,62 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class Pong extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Sprite sprite;
-	private Texture img;
 	private float speed;
 	private Music bgm;
-	
+	private Stage stage;
+
+	public class Player extends Actor {
+		Sprite sprite = new Sprite(new Texture("pongboard.jpg"));
+
+		public Player() {
+			sprite.setPosition(Gdx.graphics.getWidth() / 2  - sprite.getWidth() / 2, 0);
+		}
+
+		@Override
+		public void draw(Batch batch, float alpha) {
+			sprite.draw(batch);
+		}
+
+		@Override
+		public void act(float delta) {
+			if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+				sprite.translateX(-1.0f * speed);
+			}
+			else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+				sprite.translateX(speed);
+			}
+		}
+	}
+
+	public class Enemy extends Actor {
+		Sprite sprite = new Sprite(new Texture("pongboard.jpg"));
+
+		public Enemy() {
+			sprite.setPosition(Gdx.graphics.getWidth() / 2  - sprite.getWidth() / 2, Gdx.graphics.getHeight() - sprite.getHeight());
+		}
+
+		@Override
+		public void draw(Batch batch, float alpha) {
+			sprite.draw(batch);
+		}
+	}
+
 	@Override
 	public void create () {
-		float width = Gdx.graphics.getWidth();
-		float height = Gdx.graphics.getHeight();
-		batch = new SpriteBatch();
-		img = new Texture("pongboard.jpg");
-		sprite = new Sprite(img);
-		sprite.setPosition(width / 2 - (sprite.getWidth() / 2), 0);
+		Player player = new Player();
+		Enemy enemy = new Enemy();
+		stage = new Stage();
+		stage.addActor(player);
+		stage.addActor(enemy);
 
 		speed = 5.0f;
 
@@ -36,23 +74,13 @@ public class Pong extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-			sprite.translateX(-1.0f * speed);
-		}
-		else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-			sprite.translateX(speed);
-		}
-
-
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
+		stage.act();
+		stage.draw();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		stage.dispose();
 		bgm.dispose();
 	}
 }
